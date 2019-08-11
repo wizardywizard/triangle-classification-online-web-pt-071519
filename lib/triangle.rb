79f38169e105a8eb49c7401ee2d53d068d953c62
David Little <littledavid175@gmail.com>
2019-08-11 23:02:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Triangle
   
   attr_accessor :side_a, :side_b, :side_c
@@ -9,10 +10,12 @@ class Triangle
   end
   
   def kind 
+    binding.pry
     if @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a < @side_b && @side_b == @side_c
       :isosceles
+      
     end
   end
   
