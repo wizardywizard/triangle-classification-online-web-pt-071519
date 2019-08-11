875d0494b56d13a4fd8c8374b670608d6898202c
David Little <littledavid175@gmail.com>
2019-08-11 23:03:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
   
   def kind 
-    binding.pry
+  #  binding.pry
     if @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a < @side_b && @side_b == @side_c
