cf45b2aa1ff9fadbf65c51d430137dc27dedc9e8
David Little <littledavid175@gmail.com>
2019-08-11 23:21:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   end
   
   def kind 
-    trinagel?
+    triangel?
     if @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_b == @side_c ||@side_a == @side_c
@@ -20,7 +20,7 @@ class Triangle
     end
   end
   
-  def trinagel?
+  def triangel?
     if !(@side_a > 0 && @side_b > 0 && @side_c > 0)
       raise TriangleError
     elsif @side_a + @side_b > @side_c || @side_b + @side_c > @side_a || @side_a + @side_c > @side_b
