64a51113b65df555e9bd4c27f542c50d40891072
David Little <littledavid175@gmail.com>
2019-08-11 23:05:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
   #  binding.pry
     if @side_a == @side_b && @side_b == @side_c
       :equilateral
-    elsif @side_a < @side_b && @side_b == @side_c
+    elsif @side_a == @side_b || @side_b == @side_c ||@side_a == @side_c
       :isosceles
       
     end
