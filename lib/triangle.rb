b8ebc54a3aec433104da38e98faf5a0fb9001acc
David Little <littledavid175@gmail.com>
2019-08-11 23:18:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
   end
   
   def trinagel?
-    if !@side_a > 0 && @side_b > 0 && @side_c > 0
+    if !(@side_a > 0 && @side_b > 0 && @side_c > 0)
       raise TriangleError
     elsif @side_a + @side_b > @side_c || @side_b + @side_c > @side_a || @side_a + @side_c > @side_b
     raise TriangleError
